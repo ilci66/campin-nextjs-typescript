ba8965cd27ca866646d22d3ea07107701486745d
ilci66 <akbiyik.ilker7@gmail.com>
2021-10-14 12:52:06 +0300

    added sign up page

---
+++ pages/sign-up.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const SignUp: NextPage = () => {
+    return(
+        <div>
+            <h1>Here will be the sign up form</h1>
+            
+        </div>
+    )
+}
+export default SignUp
