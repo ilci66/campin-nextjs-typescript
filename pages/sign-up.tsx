aca752bf2ca0e9b46923acd004ac0f3963b86f92
ilci66 <akbiyik.ilker7@gmail.com>
2021-10-15 10:52:56 +0300

    working on sign up page

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,11 +1,86 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const SignUp: NextPage = () => {
-    return(
-        <div>
-            <h1>Here will be the sign up form</h1>
-            
+    return(<>
+    <Head>
+        <title>Sign Up | Campin'</title>
+        <meta name="description" content="Sign Up Page" />
+        <link rel="icon" href="/favicon-c.ico" />
+    </Head>
+        <div className="sign-up-container">
+            <div className="sign-up-form-container">
+                <div className="sign-up-pic">
+                    <img src="/vertical-camping-pic.jpg" alt="2 campers with a nice view" />
+                </div>
+                <div className="sign-up-form"></div>
+                    <form action="" className="">
+                    <h1>Here will be the sign up form</h1>
+                        <div className="input-container">
+                            <input 
+                                id="sign-in-email" 
+                                className="input" 
+                                type="text" 
+                                autoComplete="email" 
+                                placeholder=" "
+                                required 
+                            />
+                            <div className="cut cut-short">   
+                                <label htmlFor="email" className="place">Email</label>
+                            </div>
+                        </div>
+                        <div className="input-container">
+                        <input 
+                            id="sign-in-password" 
+                            className="input" 
+                            type="password" 
+                            required 
+                            />
+                            <div className="cut">
+                                <label htmlFor="password" className="placeholder">Password</label>
+                            </div>
+                        </div>
+                        <div className="input-container">
+                        <input 
+                            id="sign-in-password" 
+                                className="input" 
+                                type="password" 
+                                required 
+                            />
+                            <div className="cut">
+                                <label htmlFor="password" className="placeholder">Password</label>
+                            </div>
+                        </div>
+                    </form>
+            </div>
         </div>
-    )
+        <style jsx>{`
+            .sign-up-container{
+                background-color: gray;
+                top: 0;
+                left:0;
+                width:100%;
+                height100%;
+                position:relative;
+                display:flex;
+                align-items: center;
+                justifiy-content: center;
+            }
+            .sign-up-form-container{
+                max-width: 960px;
+                display:flex;
+                width:80%;
+                margin: 50px; auto
+                align-items:center;
+                justify-content:center;
+            }
+            .sign-up-pic{
+                width: 42%;
+            }
+            .sign-up-form{
+                border: 2px solid red;
+            }
+        `}</style>
+    </>)
 }
 export default SignUp
