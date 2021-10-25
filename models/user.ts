ad8853ba67f564c35c48a60871d5191329ebc6e3
unknown <akbiyik.ilker7@gmail.com>
2021-10-25 12:27:07 +0200

    fixed save error & cleaned up comments

--- models/user.ts
+++ models/user.ts
@@ -13,33 +13,6 @@ const userSchema = new Schema<User>({
   });
   
 
-// const UserModel = model<User>('User', userSchema);
-
-// export default UserModel;
-
-//tryting to fix the overwrite error I keep getting
- 
-// module.exports = model<User>('User', schema);
-
-// const UserModel = mongoose.models.UserModel || model<User>('User', userSchema);
-// console.log(UserModel)
-
-// module.exports = model<User>('User', userSchema);
-
-
-//Wrote this to fix the overwrite error but now it's causing another error
-// let UserModel;
-// try {
-//   UserModel = model<User>('users')
-// } catch (error) {
-//   UserModel = model<User>('users', userSchema)
-// }
-
-
-// export default UserModel
-
-
-
 try {
   module.exports = model<User>('User');
 }catch(error){
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -5,10 +5,6 @@ import { isNativeError } from 'util/types'
 import { connectToDatabase } from '../../lib/connection'
 const UserModel = require('../../models/user')
 
-//tyring to fix the overwrite error ==> fixed it with the try catch block in model file
-// const mongoose = require('mongoose')
-// const UserModel = require('../../models/user')
-
 const bcrypt = require('bcrypt')
 
 type Data = {
@@ -21,9 +17,8 @@ export default async function handler(
 ) {
     let connected = await connectToDatabase();
 
-    // console.log("request made",req.body)
-
     const { username, email, password } = req.body
+
     if(connected){
             console.log("connected to database")
             switch(req.method) {
@@ -33,21 +28,19 @@ export default async function handler(
             break;
             case 'POST': {
                 console.log("request to create a user")
-                UserModel.find({email: email}, async (err:object, data:object) => {
+                UserModel.findOne({email: email}, async (err:object, data:object) => {
                     if(err){
                         console.log("error in database", err)
                         return;
-                    }
-                    // else if(data){
-                    //     console.log(data, "there's an account with that email address")
-                    //     return;
-                    // }
-                    else{
+                    }else if(data){
+                        console.log(data, "there's an account with that email address")
+                        return;
+                    }else if(!data){
                         console.log("Time to create the user")
                         try {
                             const hash = await bcrypt.hash(password, 10);
                             let newUser = await new UserModel({
-                                username: username,
+                                name: username,
                                 email: email,
                                 password: hash
                             })
@@ -56,21 +49,6 @@ export default async function handler(
                                 console.log("saved ==>", data)
                                 return res.status(201);
                             })
-                            // bcrypt.hash(password, 10, (err:string, hash:string) => {
-                            //     if(err){ throw new Error(err)}
-
-                            //     console.log("no error gonna salt now")
-                                
-                            //     let newUser = new UserModel({
-                            //         username: username,
-                            //         email: email,
-                            //         password: hash
-                            //     })
-                            //     newUser.save((err, data:object) => {
-                            //         console.log("saved ==>", data)
-                            //         res.status(201)
-                            //     })
-                            // });
                         } catch (error) {
                             console.log("error ==>",error)
                             return;
