425596983a4f0e9874976fb37b3409503d7d696b
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-09 09:59:46 +0100

    updated

--- models/marker.ts
+++ models/marker.ts
@@ -5,14 +5,24 @@ interface Marker {
   lng: number;
   type: string;
   description: string;
+  addedBy: string;
+  createdAt: number;
 }
 
-const markerSchema = new Schema<Marker>({
-  lat: {type: Number, required: true},
-  lng: { type: Number, required: true },
-  type: { type: String, required: true },
-  description: { type: String, required: true }
-});
+const markerSchema = new Schema<Marker>(
+  {
+    lat: {type: Number, required: true},
+    lng: { type: Number, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: true },
+    addedBy: { type: String, required: true },
+    // createdAt: { type: Number, required: true}
+  }, 
+  // didn't work for some reason, as I don't need any update date or anything , cancelling it
+  {
+    timestamps: true
+  }
+);
   
 
 try {
