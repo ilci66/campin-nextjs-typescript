75a0cc66f48e01c8d3eec38c056a89feccbf6b1c
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-13 15:26:05 +0100

    working on the query

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,8 +1,11 @@
-import type { NextPage } from 'next'
+// import { gql } from '@apollo/client'
+import { GraphQLClient, gql } from "graphql-request";
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import client from '../lib/apolloClient';
 
 
-const Posts: NextPage = () => {
+const Posts: NextPage = ( { blogs } ) => {
     return(<>
     <Head>
     <title>Posts | Campin'</title>
@@ -11,6 +14,9 @@ const Posts: NextPage = () => {
     </Head>
     <div className="posts-container">
         <h1>this page will contain all of the posts and their short descpitions or at least list of the headers along with the thumbnails</h1>
+        <ul>
+            {blogs.map((blog: {title: string}, i: number | string) => <li key={i}>{blog.title}</li> )}
+        </ul>
     </div>
         <style jsx>{`
             .posts-container {
@@ -19,4 +25,43 @@ const Posts: NextPage = () => {
         `}</style>
     </>)
 }
-export default Posts
+export default Posts;
+
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL!);
+    const query = gql`
+        query {
+            blogPosts {
+                title
+                slug
+            }
+        }
+    `;
+    const data = await client.request(query);
+
+    return {
+        props: { blogs: data },
+    };
+
+    // This looks strange and I wouldn't be able to remember in a million years so gonna use another module
+    // const { data: blogs } = await client.query({
+    //     query : gql`
+    //     query blogPosts{
+    //         title
+    //         wideThumbnail
+    //         slug
+    //         description
+    //         date
+    //         inPostImages
+    //         richText
+    //         }
+    //     ` 
+    // })
+        
+    // console.log(blogs);
+
+
+    
+}
