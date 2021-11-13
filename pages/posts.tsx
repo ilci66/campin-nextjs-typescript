afbba09b4a142ace3e38fe8dfc525a22402a9032
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-13 14:09:48 +0100

    started the posts

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+
 
 const Posts: NextPage = () => {
     return(<>
+    <Head>
+    <title>Posts | Campin'</title>
+        <meta name="description" content="All Posts and Short Descriptions" />
+        <link rel="icon" href="/favicon-c.ico" />
+    </Head>
     <div className="posts-container">
-        <h1>this page will contain all the posts</h1>
+        <h1>this page will contain all of the posts and their short descpitions or at least list of the headers along with the thumbnails</h1>
     </div>
         <style jsx>{`
             .posts-container {
