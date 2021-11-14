fa3e5497cc5d750d59af0abe82202984083a3fc8
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-14 13:17:03 +0100

    error fixed

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -4,8 +4,9 @@ import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import client from '../lib/apolloClient';
 
-
-const Posts: NextPage = ( { blogs } ) => {
+// will change the type later
+const Posts: NextPage = ( { blogs }:any ) => {
+    console.log("blogs ==>",blogs)
     return(<>
     <Head>
     <title>Posts | Campin'</title>
@@ -15,7 +16,7 @@ const Posts: NextPage = ( { blogs } ) => {
     <div className="posts-container">
         <h1>this page will contain all of the posts and their short descpitions or at least list of the headers along with the thumbnails</h1>
         <ul>
-            {blogs.map((blog: {title: string}, i: number | string) => <li key={i}>{blog.title}</li> )}
+            {/* {blogs.map((blog: {title: string}, i: number | string) => <li key={i}>{blog.title}</li> )} */}
         </ul>
     </div>
         <style jsx>{`
@@ -29,22 +30,23 @@ export default Posts;
 
 
 export const getStaticProps: GetStaticProps = async () => {
-
+    console.log("graphcms url", process.env.NEXT_PUBLIC_GRAPHCMS_URL)
     const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL!);
+    console.log("client ==>", client)
+    // well the query worked in api playground but still getting the same error, looking into it
     const query = gql`
-        query {
-            blogPosts {
+        query BlogPost{
+            blogPosts{
                 title
-                slug
             }
         }
     `;
-    const data = await client.request(query);
+    const data:{blogPosts:[]} = await client.request(query);
 
     return {
-        props: { blogs: data },
+        props: { blogs: data.blogPosts },
     };
-
+    // return { props: { blogs:  [{title : "test"}]}}
     // This looks strange and I wouldn't be able to remember in a million years so gonna use another module
     // const { data: blogs } = await client.query({
     //     query : gql`
